2293c4b712fb79a5adbe2887bd0cc3035f8eb73f
mohankhanth <mohankhanth@gmail.com>
2021-05-28 11:12:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-registerform01
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-registerform01)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularforms Mohan swamy';
+  constructor(private _flashMessagesService: FlashMessagesService) {}
+  onSubmit(form: any) {
+    console.log(form.value);
+    this._flashMessagesService.show('Form registered successs...', {
+      cssClass: 'alert-success',
+      timeout: 5000
+    });
+    form.reset({});
+  }
+}
